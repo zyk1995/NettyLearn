be563329b10bb60487463ca0e7719ca3913199f5
zyk <1251134350@qq.com>
2020-08-17 18:53:09 +0800

    【nio netty】

--- src/main/java/com/zyk/netty/nio/TimeClientHandle.java
+++ src/main/java/com/zyk/netty/nio/TimeClientHandle.java
@@ -40,16 +40,16 @@ public class TimeClientHandle implements Runnable {
     private volatile boolean stop;
 
     public TimeClientHandle(String host, int port) {
-	this.host = host == null ? "127.0.0.1" : host;
-	this.port = port;
-	try {
-	    selector = Selector.open();
-	    socketChannel = SocketChannel.open();
-	    socketChannel.configureBlocking(false);
-	} catch (IOException e) {
-	    e.printStackTrace();
-	    System.exit(1);
-	}
+		this.host = host == null ? "127.0.0.1" : host;
+		this.port = port;
+		try {
+			selector = Selector.open();
+			socketChannel = SocketChannel.open();
+			socketChannel.configureBlocking(false);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
     }
 
     /*
@@ -103,36 +103,36 @@ public class TimeClientHandle implements Runnable {
 
     private void handleInput(SelectionKey key) throws IOException {
 
-	if (key.isValid()) {
-	    // 判断是否连接成功
-	    SocketChannel sc = (SocketChannel) key.channel();
-	    if (key.isConnectable()) {
-			if (sc.finishConnect()) {
-				sc.register(selector, SelectionKey.OP_READ);
-				doWrite(sc);
-			} else {
-				System.exit(1);// 连接失败，进程退出
+    	if (key.isValid()) {
+    		// 判断是否连接成功
+			SocketChannel sc = (SocketChannel) key.channel();
+			if (key.isConnectable()) {
+				if (sc.finishConnect()) {
+					sc.register(selector, SelectionKey.OP_READ);
+					doWrite(sc);
+				} else {
+					System.exit(1);// 连接失败，进程退出
+				}
 			}
-	    }
-	    if (key.isReadable()) {
-			ByteBuffer readBuffer = ByteBuffer.allocate(1024);
-			int readBytes = sc.read(readBuffer);
-			if (readBytes > 0) {
-				readBuffer.flip();
-				byte[] bytes = new byte[readBuffer.remaining()];
-				readBuffer.get(bytes);
-				String body = new String(bytes, "UTF-8");
-				System.out.println("Now is : " + body);
-				this.stop = true;
-			} else if (readBytes < 0) {
-				// 对端链路关闭
-				key.cancel();
-				sc.close();
-			} else {
-				; // 读到0字节，忽略
+			if (key.isReadable()) {
+				ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+				int readBytes = sc.read(readBuffer);
+				if (readBytes > 0) {
+					readBuffer.flip();
+					byte[] bytes = new byte[readBuffer.remaining()];
+					readBuffer.get(bytes);
+					String body = new String(bytes, "UTF-8");
+					System.out.println("Now is : " + body);
+					this.stop = true;
+				} else if (readBytes < 0) {
+					// 对端链路关闭
+					key.cancel();
+					sc.close();
+				} else {
+						; // 读到0字节，忽略
+				}
 			}
-	    }
-	}
+    	}
 
     }
 
